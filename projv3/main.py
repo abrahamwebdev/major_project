88b311e2a3c0c3f6b5192f4954342d0e248a2c77
abraham <agthankachan007@gmail.com>
2022-04-21 19:19:41 +0530

    result added

--- projv3/main.py
+++ projv3/main.py
@@ -9,19 +9,12 @@ from tensorflow.keras.models import load_model
 app = Flask(__name__)
 
 model = keras.models.load_model('./mnist_classification.h5')
-randoml=[]
 
 @app.route("/home")
 @app.route("/",methods=['GET'])
 def home():
-    if len(randoml)>0:
-        randoml.clear()
-    else:
-        rando=random.randint(0, 9)
-        randoml.append(rando)
-    random_no=randoml[0]
-    print(f"first random : {str(random_no)}")
-    return render_template("home.html",random_no=random_no)
+	random_no=random.randint(0, 9)
+	return render_template("home.html",random_no=random_no)
 
 @app.route('/', methods=['POST'])
 def canvas():
@@ -45,21 +38,9 @@ def canvas():
     try:
         prediction = np.argmax(model.predict(img))
         print(f"Prediction Result : {str(prediction)}")
-        pred=int(prediction)
-        random_no=randoml[0]
-        print(f"Second random no : {str(random_no)}")
-        if pred ==random_no:
-            response="Successfully Solved"
-        else:
-            response="Captcha is wrong"
-        randoml.pop()
-        return redirect(url_for("predict", response=response))
-        # return render_template('result.html', response=prediction, success=True)
+        return render_template('home.html', response=str(prediction), canvasdata=canvasdata, success=True)
     except Exception as e:
-        return render_template('home.html', response=str(e))
+        return render_template('home.html', response=str(e), canvasdata=canvasdata)
 
-@app.route("/<response>")
-def predict(response):
-    return render_template("result.html", response=response)
 if __name__ == "__main__":
     app.run(debug=True)
